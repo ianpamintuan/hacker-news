5269d560bf8797d0bb89e922c41aee9d504aec91
Mark Ian Pamintuan <ianpamintuan06@gmail.com>
2021-03-22 20:16:44 +0800

    Build story component. Add styling.

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -2,13 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-export const Story = ({ className, children }) => {
+export const Story = ({ className, data }) => {
+    const { title, points, author, num_comments, url } = data;
     return (
-        <article className={classNames("story", className)}>{children}</article>
+        <article className={classNames("story", className)}>
+            <h4 className="title">{title}</h4>
+            <p className="info">{`${points} points by ${author} | ${num_comments} comments`}</p>
+            <div className="links">
+                <a className="text-link" href={url} target="_blank" rel="noreferrer">
+                    Read more
+                </a>
+            </div>
+        </article>
     );
 };
 
 Story.propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node.isRequired,
+    title: PropTypes.string,
 };
