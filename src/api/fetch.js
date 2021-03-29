a8af94aa706c26cf44102dd1aa76626dd73fb410
Mark Ian Pamintuan <ianpamintuan06@gmail.com>
2021-03-29 20:41:27 +0800

    Add fetch abort controller

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -6,19 +6,24 @@ export const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setIsLoading(true);
-        fetch(url)
+        fetch(url, { signal: abortCont.signal })
             .then((response) => response.json())
             .then((data) => {
                 setList(data);
                 setError(null);
             })
             .catch((error) => {
-                console.log(error);
-                setError(error);
-                setList(null);
+                if (error.name !== "AbortError") {
+                    setError(error.message);
+                    setList(null);
+                }
             })
             .finally(() => setIsLoading(false));
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { list, loading, error };
